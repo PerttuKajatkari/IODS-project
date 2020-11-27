2d06e386b61933be17a3cd2e5666d99fd30fd9a2
Perttu Kajatkari <perttu.kajatkari@helsinki.fi>
2020-11-27 03:52:19 +0200

    exercise 5 commit

--- create_human.R
+++ create_human.R
@@ -1,5 +1,8 @@
 #IODS Exercise 5 data wrangling/creation script. Perttu Kajatkari, 20.11.2020
 
+library(dplyr)
+library(tidyr)
+library(stringr)
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
 
@@ -18,9 +21,9 @@ summary(gii)
 
 #rename variables
 colnames(gii)
-hd <- setNames(hd, c('HDI_rank','country', 'HDI','life_exp','exp_edu','mean_edu','GNI','GNI-HDI'))
+hd <- setNames(hd, c('HDI_rank','country', 'HDI','life.exp','exp.edu','mean_edu','GNI','GNI-HDI'))
 colnames(hd)
-gii <- setNames(gii, c('GII_rank','country','GII','mortality','ado_birth_rate','parl_rep','edu2F','edu2M','labourF','labourM'))
+gii <- setNames(gii, c('GII_rank','country','GII','mat.mort','ado.birth','parl.rep','edu2F','edu2M','labourF','labourM'))
 colnames(gii)
 
 #join the two datasets
@@ -29,3 +32,32 @@ human$edu2R <- human$edu2F/human$edu2M
 human$labourR <- human$labourF/human$labourM
 #should be 195 obs of 19 variables!
 str(human)
+
+human$GNI=str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric
+
+human <- select(human,c("country","edu2F","labourF", "exp.edu", "life.exp","GNI","mat.mort","ado.birth","parl.rep"))
+str(human)
+#human <- filter(human, TRUE)
+
+#remove rows with NA
+human <- human %>% drop_na()
+
+#check the data for area removal
+tail(human,10)
+last <- nrow(human) - 7
+human = human[1:last,]
+
+rownames(human) <- human$country
+human <- select(human, -country)
+
+#check that all is as wanted
+tail(human,10)
+str(human)
+
+#write the dataset to a file
+write.table(human, file="./data/human.txt")
+
+#and test that the file is readable
+hunam <- read.table("./data/human.txt",header=TRUE)
+str(hunam)
+
